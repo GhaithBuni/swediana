9619136573ef03f2a08622ee39b7875f902637f9
Ghaith Buni <ghaith.buni122@gmail.com>
2025-08-27 15:46:13 +0200

    intial commit

---
+++ src/app/kontakt/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Kontakt</div>;
+};
+
+export default page;
---
+++ src/app/omOss/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Om oss</h1>
+      <p>Välkommen till vår sida om oss!</p>
+    </div>
+  );
+};
+
+export default Page;
